df3caab968144fd448ead72be6a3a3e63a70b168
Joseph <jossosborne@gmail.com>
2018-02-12 13:56:51 -0800

    clean

--- addition.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class addition
-{
-public:
-	addition();
-	~addition();
-	double add(); //prompts user for 2 values, prints result, returns result
-};
-
--- subtraction.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class subtraction
-{
-public:
-	subtraction();
-	~subtraction();
-	double subtract(); //prompts user for 2 values, prints result, returns result
-};
-
